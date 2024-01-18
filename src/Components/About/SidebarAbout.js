5e1bbe1820919934abe2d46de837b28e7b6b0bce
Eduardo Miranda <eduardosilvamiranda1910@gmail.com>
2024-01-18 14:34:42 +0000

    minor changes in the about me page

---
+++ src/Components/About/SidebarAbout.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function SidebarAbout () {
+  return (
+    <div className="sidebarAbout">
+      <a href="#me" className="rotated-letter linkAbout">Me</a>
+      <a href="#skills" className="rotated-letter linkAbout">Skills</a>
+      <a href="#AcademicJourney" className="rotated-letter linkAbout">Academic Journey</a>
+      <a href="#Interests" className="rotated-letter linkAbout">Interests</a>
+    </div>
+  );
+}
+
+export default SidebarAbout;
